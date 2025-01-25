90b8e0f17ed6fcb6ad7a51c230be20c8d92fe99b
DidierPereira <krrattoss5@gmail.com>
2025-01-25 18:39:42 -0500

    (feat/dropAndDragAnimation)

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../presentation/screens/home/HomeScreen';
 import {Animation101} from '../presentation/screens/animations/Animation101';
+import {Animation102Screen} from '../presentation/screens/animations/Animation102Screen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export const StackNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="HomeScreenScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101} />
+      <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
     </Stack.Navigator>
   );
 };
