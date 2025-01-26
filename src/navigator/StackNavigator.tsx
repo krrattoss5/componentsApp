906f4c37b54947d4aebf365da0a99441d90b0dc1
DidierPereira <krrattoss5@gmail.com>
2025-01-25 19:17:46 -0500

    (feat\switches)

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../presentation/screens/home/HomeScreen';
 import {Animation101} from '../presentation/screens/animations/Animation101';
 import {Animation102Screen} from '../presentation/screens/animations/Animation102Screen';
+import {SwitchScreen} from '../presentation/screens/switches/SwitchScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="HomeScreenScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
+      <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Card} from '../../components/ui/Card';
+
+export const SwitchScreen = () => {
+  return (
+    <CustomView>
+      <Card>
+        <Text>SwitchScreen</Text>
+      </Card>
+    </CustomView>
+  );
+};
