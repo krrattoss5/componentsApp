1e09a5af1c98ae7fa0a785ef33ee3c544231dc3d
DidierPereira <krrattoss5@gmail.com>
2025-01-25 19:44:08 -0500

    (feat/Button)

---
+++ src/presentation/components/ui/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Pressable, StyleProp, Text, ViewStyle} from 'react-native';
+import {globalStyles, colors} from '../../../config/theme/theme';
+
+interface Props {
+  text: string;
+  styles?: StyleProp<ViewStyle>;
+
+  onPress: () => void;
+}
+
+export const Button = ({text, styles, onPress}: Props) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({pressed}) => [
+        globalStyles.btnPrimary,
+        {
+          opacity: pressed ? 0.8 : 1,
+          backgroundColor: colors.primary,
+        },
+      ]}>
+      <Text
+        style={[
+          globalStyles.btnPrimaryText,
+          {
+            color: colors.buttonTextColor,
+          },
+        ]}>
+        Button
+      </Text>
+    </Pressable>
+  );
+};
--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
 import {CustomView} from '../../components/ui/CustomView';
-import {Card} from '../../components/ui/Card';
 
 export const SwitchScreen = () => {
-  return (
-    <CustomView>
-      <Card>
-        <Text>SwitchScreen</Text>
-      </Card>
-    </CustomView>
-  );
+  return <CustomView style={{paddingHorizontal: 10}} />;
 };
